785ba442730b1d48f9fccceb4f860fd00231b2dd
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-03-22 20:43:39 +0500

    My Portfolio

--- Portfolio/src/Components/Links.jsx
+++ Portfolio/src/Components/Links.jsx
@@ -9,19 +9,19 @@ const Links = () => {
         <>
             <ul className='flex flex-col absolute top-4 left-0 w-[100%] items-center  lg:static lg:flex lg:flex-row gap-6 lg:gap-10 text-textColor text-lg font-bold cursor-pointer lg:text-xl'>
                 <li><NavLink to="/" className={({ isActive }) =>
-                    isActive ? 'navLinks' : 'hover:bg-secondaryBackground px-28 py-[4px] text-textColor lg:hover:bg-mainBackground lg:px-0 lg:py-0'
+                    isActive ? 'navLinks' : ' text-textColor '
                 } >Home</NavLink></li>
                 <li ><NavLink to="/about" className={({ isActive }) =>
-                    isActive ? 'navLinks' : 'hover:bg-secondaryBackground px-28 py-[4px] text-textColor lg:hover:bg-mainBackground lg:px-0 lg:py-0'
+                    isActive ? 'navLinks' : ' text-textColor '
                 }>About</NavLink></li>
                 <li ><NavLink to="/services" className={({ isActive }) =>
-                    isActive ? 'navLinks' : 'hover:bg-secondaryBackground px-28 py-[4px] text-textColor lg:hover:bg-mainBackground lg:px-0 lg:py-0'
+                    isActive ? 'navLinks' : 'text-textColor'
                 }>Services</NavLink></li>
                 <li ><NavLink to="/skills" className={({ isActive }) =>
-                    isActive ? 'navLinks' : 'hover:bg-secondaryBackground px-28 py-[4px] text-textColor lg:hover:bg-mainBackground lg:px-0 lg:py-0'
+                    isActive ? 'navLinks' : 'text-textColor'
                 }>Skills</NavLink></li>
                 <li ><NavLink to="/contact" className={({ isActive }) =>
-                    isActive ? 'navLinks' : 'hover:bg-secondaryBackground px-28 py-[4px] text-textColor lg:hover:bg-mainBackground lg:px-0 lg:py-0'
+                    isActive ? 'navLinks' : 'text-textColor'
                 }>Contact</NavLink></li>
 
             </ul>
