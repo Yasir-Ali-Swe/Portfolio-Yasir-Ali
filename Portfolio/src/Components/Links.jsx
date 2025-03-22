6f30a9c8d0c237f9d764c160f0773b1c289ea48e
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-03-22 20:40:41 +0500

    My Portfolio

--- Portfolio/src/Components/Links.jsx
+++ Portfolio/src/Components/Links.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
+
+
+
 const Links = () => {
     return (
         <>
-            <ul className='flex flex-col absolute top-4 left-0 w-full items-center  lg:static lg:flex lg:flex-row gap-4 text-textColor text-lg font-bold cursor-pointer lg:text-xl'>
+            <ul className='flex flex-col absolute top-4 left-0 w-[100%] items-center  lg:static lg:flex lg:flex-row gap-6 lg:gap-10 text-textColor text-lg font-bold cursor-pointer lg:text-xl'>
                 <li><NavLink to="/" className={({ isActive }) =>
-                    isActive ? 'navLinks' : 'text-textColor'
+                    isActive ? 'navLinks' : 'hover:bg-secondaryBackground px-28 py-[4px] text-textColor lg:hover:bg-mainBackground lg:px-0 lg:py-0'
                 } >Home</NavLink></li>
                 <li ><NavLink to="/about" className={({ isActive }) =>
-                    isActive ? 'navLinks' : 'text-textColor'
+                    isActive ? 'navLinks' : 'hover:bg-secondaryBackground px-28 py-[4px] text-textColor lg:hover:bg-mainBackground lg:px-0 lg:py-0'
                 }>About</NavLink></li>
                 <li ><NavLink to="/services" className={({ isActive }) =>
-                    isActive ? 'navLinks' : 'text-textColor'
+                    isActive ? 'navLinks' : 'hover:bg-secondaryBackground px-28 py-[4px] text-textColor lg:hover:bg-mainBackground lg:px-0 lg:py-0'
                 }>Services</NavLink></li>
                 <li ><NavLink to="/skills" className={({ isActive }) =>
-                    isActive ? 'navLinks' : 'text-textColor'
+                    isActive ? 'navLinks' : 'hover:bg-secondaryBackground px-28 py-[4px] text-textColor lg:hover:bg-mainBackground lg:px-0 lg:py-0'
                 }>Skills</NavLink></li>
                 <li ><NavLink to="/contact" className={({ isActive }) =>
-                    isActive ? 'navLinks' : 'text-textColor'
+                    isActive ? 'navLinks' : 'hover:bg-secondaryBackground px-28 py-[4px] text-textColor lg:hover:bg-mainBackground lg:px-0 lg:py-0'
                 }>Contact</NavLink></li>
 
             </ul>
