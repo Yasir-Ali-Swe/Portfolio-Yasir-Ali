b4445776542f85aa4aece6cf4a4c0f91ff485624
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-03-23 16:07:55 +0500

    My Portfolio

--- Portfolio/src/Pages/About.jsx
+++ Portfolio/src/Pages/About.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div className='h-screen w-[80%] mx-auto flex justify-center items-center'>
-            <h1 className='text-textColor'>About Page</h1>
+        <div className='w-[80%] mx-auto flex flex-col justify-center items-center gap-3 lg:flex-row lg:gap-7 pt-14 lg:pt-20 '>
+            <div className="profileContainer text-center text-textColor w-[100%] lg:w-[35%] border-2 border-selected">
+                <h1>profile</h1>
+            </div>
+            <div className="aboutContainer text-center text-textColor w-[100%] lg:w-[65%] border-2 border-selected">
+                <h1>about</h1>
+            </div>
         </div>
     )
 }
